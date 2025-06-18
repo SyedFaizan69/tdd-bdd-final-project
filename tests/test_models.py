7b0c057d6de2a58c325cd773288f21b8b91f474c
SyedFaizan69 <faizan.syedshah@gmail.com>
2025-06-18 11:34:40 +0500

    Add FIND BY NAME test case to test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,7 @@
-def test_list_all_products(self):
-    """Test listing all products"""
-    for _ in range(3):
-        product = ProductFactory()
-        product.create()
-    results = Product.all()
-    self.assertEqual(len(results), 3)
+def test_find_by_name(self):
+    """Test finding products by name"""
+    product = ProductFactory(name="Wrench")
+    product.create()
+    results = Product.find_by_name("Wrench").all()
+    self.assertGreaterEqual(len(results), 1)
+    self.assertEqual(results[0].name, "Wrench")
